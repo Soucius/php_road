4950e4ab561ede178024f8df2d029f58d5882e92
Soucius <soucius.ggwp@gmail.com>
2025-02-15 19:22:21 +0300

    SQL Injection security

--- security/security.php
+++ security/security.php
@@ -1,3 +1,19 @@
+<?php
+    include "config/config.php";
+
+    $sql = "insert into kullanicilar set isim='{$_POST['isim']}', soyisim='{$_POST['soyisim']}'";
+
+    if (isset($_POST["isim"])) {
+        $insert = $db->query($sql);
+
+        if ($insert) {
+            echo "eklendi";
+        } else {
+            echo "Bir hata olustu";
+        }
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,13 +26,10 @@
     <h1>Guvenlik</h1>
 
     <form action="" method="post">
-        <input type="text" name="username"> <br>
+        <input type="text" name="isim"></input> <br>
+        <input type="text" name="soyisim"></input> <br>
 
         <button type="submit">Submit</button>
     </form>
-
-    <?php
-        echo htmlspecialchars($_POST['username'] ?? '');
-    ?>
 </body>
 </html>
